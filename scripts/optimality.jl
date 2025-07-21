3a25517fb900b3f505401784401b1d5156958bb2
Brendan Wallace <bwallace722@gmail.com>
2025-07-21 09:40:39 -0700

    Update to run multiple search-strategy logic

--- scripts/optimality.jl
+++ scripts/optimality.jl
@@ -8,7 +8,7 @@ import Paths
 
 # Invariant.
 patchLogics = [Paths.LINEAR]
-searchStrategies = [Paths.KANAI_SUZUKI]
+searchStrategies = [Paths.GRID_WALK_MOORE, Paths.GRID_WALK_NEUMANN]
 
 boundaryConditions = [Paths.SOLID, Paths.PERIODIC]
 seriesName = "optimality"
@@ -22,15 +22,29 @@ SAVE_HEADINGS = false
 SAVE_PATCHES = true
 
 
-# 7/8 Run to look at optimality vs time
-NUM_REPLICAS = 4
+improvementRatios: [
+	10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0,
+	90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0,
+	160.0, 170.0, 180.0, 190.0, 200.0,
+]
+NUM_REPLICAS = 8
 LOCATION_CONFIGURATIONS = [1, 2]
 numLocations = [10]
 F = 100
 upf = 100
 maxCosts = [2.0, 8.0]
-improvementRatios = [50.0, 100.0, 150.0, 200.0]
 PRs = [0.001]
 boundaryConditions = [Paths.SOLID]
+
+# # 7/8 Run to look at optimality vs time
+# NUM_REPLICAS = 4
+# LOCATION_CONFIGURATIONS = [1, 2]
+# numLocations = [10]
+# F = 100
+# upf = 100
+# maxCosts = [2.0, 8.0]
+# improvementRatios = [50.0, 100.0, 150.0, 200.0]
+# PRs = [0.001]
+# boundaryConditions = [Paths.SOLID]
 
 include("runs.jl")
--- scripts/runs.jl
+++ scripts/runs.jl
@@ -12,7 +12,7 @@ function main()
     # end
 
     # COMFORTS = Dict(2.0 => 0.3, 10.0 => 0.5, 5.0 => 0.4)
-    FOLDER = "data/series/$(seriesName)|$(today())"
+    FOLDER = "data/$(seriesName)|$(today())"
     datafile = "$(FOLDER)/data.json"
     mkpath("$(FOLDER)")
 
@@ -27,79 +27,83 @@ function main()
 
     i = 1
     totalI = (length(maxCosts) * length(PRs) * length(improvementRatios) * length(numLocations)
-              * length(patchLogics) * length(boundaryConditions) * NUM_REPLICAS * length(LOCATION_CONFIGURATIONS))
+              * length(patchLogics) * length(boundaryConditions) * NUM_REPLICAS * length(LOCATION_CONFIGURATIONS)
+              * length(searchStrategies)
+              )
 
-    for maxCost ∈ maxCosts
-        for ratio ∈ improvementRatios
-            for boundaryCondition ∈ boundaryConditions
-                # if searchStrategy == Paths.GRADIENT_WALKER
-                # comfort = COMFORTS[maxCost]
-                # end
-                for pR ∈ PRs
-                    for patchLogic ∈ patchLogics
-                        pI = pR * ratio
+    for searchStrategy ∈ searchStrategies
+        for maxCost ∈ maxCosts
+            for ratio ∈ improvementRatios
+                for boundaryCondition ∈ boundaryConditions
+                    # if searchStrategy == Paths.GRADIENT_WALKER
+                    # comfort = COMFORTS[maxCost]
+                    # end
+                    for pR ∈ PRs
+                        for patchLogic ∈ patchLogics
+                            pI = pR * ratio
 
-                        for nLocations ∈ numLocations
+                            for nLocations ∈ numLocations
 
-                            for locationSeed ∈ LOCATION_CONFIGURATIONS
+                                for locationSeed ∈ LOCATION_CONFIGURATIONS
 
 
-                                for replica ∈ 1:NUM_REPLICAS
+                                    for replica ∈ 1:NUM_REPLICAS
 
-                                    settings = Paths.Settings(
-                                        maxCost=maxCost,
-                                        scenario=Paths.RANDOM_FIXED,
-                                        # searchStrategy=searchStrategy,
-                                        patchImprovement=pI,
-                                        patchRecovery=pR,
-                                        improvementLogic=patchLogic,
-                                        recoveryLogic=patchLogic,
-                                        numSteinerPoints=2,
-                                        randomSeedWalkers=replica,
-                                        randomSeedLocations=locationSeed,
-                                        boundaryConditions=boundaryCondition,
-                                        numLocations=nLocations,
-                                        # comfortWeight=comfort,
-                                    )
+                                        settings = Paths.Settings(
+                                            maxCost=maxCost,
+                                            scenario=Paths.RANDOM_FIXED,
+                                            searchStrategy=searchStrategy,
+                                            patchImprovement=pI,
+                                            patchRecovery=pR,
+                                            improvementLogic=patchLogic,
+                                            recoveryLogic=patchLogic,
+                                            numSteinerPoints=2,
+                                            randomSeedWalkers=replica,
+                                            randomSeedLocations=locationSeed,
+                                            boundaryConditions=boundaryCondition,
+                                            numLocations=nLocations,
+                                            # comfortWeight=comfort,
+                                        )
 
-                                    simulationResult = Paths.SimulationResult(settings)
-                                    push!(simulationResults, simulationResult)
+                                        simulationResult = Paths.SimulationResult(settings)
+                                        push!(simulationResults, simulationResult)
 
-                                    sim::Paths.Simulation = Paths.MakeSimulation(settings)
-                                    push!(simulationResult.snapshots, Paths.takeSnapshot(sim))
+                                        sim::Paths.Simulation = Paths.MakeSimulation(settings)
+                                        push!(simulationResult.snapshots, Paths.takeSnapshot(sim))
 
-                                    for f ∈ 1:F
-                                        print("\r$(nLocations)/$(numLocations) $(boundaryCondition)/$(boundaryConditions) $(maxCost)/$(maxCosts) $(pR)/$(PRs) $(ratio)/$(improvementRatios) $(patchLogic) $(i)/$(totalI) $(f)/$(F)")
+                                        for f ∈ 1:F
+                                            print("\r$(nLocations)/$(numLocations) $(boundaryCondition)/$(boundaryConditions) $(maxCost)/$(maxCosts) $(pR)/$(PRs) $(ratio)/$(improvementRatios) $(patchLogic) $(i)/$(totalI) $(f)/$(F)")
 
-                                        for u ∈ 1:upf
-                                            Paths.update!(sim)
+                                            for u ∈ 1:upf
+                                                Paths.update!(sim)
+                                            end
+
+                                            # We should always save all the information for the final snapshot.
+                                            if f == F
+                                                push!(simulationResult.snapshots, Paths.takeSnapshot(sim,
+                                                    shortestpaths=true,
+                                                    savepaths=true,
+                                                    saveheadings=true,
+                                                    savepatches=true,
+                                                ))
+                                            else
+                                                push!(simulationResult.snapshots, Paths.takeSnapshot(sim,
+                                                    shortestpaths=SHORTEST_PATHS,
+                                                    savepaths=SAVE_PATHS,
+                                                    saveheadings=SAVE_HEADINGS,
+                                                    savepatches=SAVE_PATCHES,
+                                                ))
+                                            end
+
                                         end
 
-                                        # We should always save all the information for the final snapshot.
-                                        if f == F
-                                            push!(simulationResult.snapshots, Paths.takeSnapshot(sim,
-                                                shortestpaths=true,
-                                                savepaths=true,
-                                                saveheadings=true,
-                                                savepatches=true,
-                                            ))
-                                        else
-                                            push!(simulationResult.snapshots, Paths.takeSnapshot(sim,
-                                                shortestpaths=SHORTEST_PATHS,
-                                                savepaths=SAVE_PATHS,
-                                                saveheadings=SAVE_HEADINGS,
-                                                savepatches=SAVE_PATCHES,
-                                            ))
+                                        # This writes out the intermediate data every run (I think).
+                                        serialize(datafile, simulationResults)
+                                        open(datafile, "w") do io
+                                            JSON3.write(io, simulationResults)
                                         end
-
+                                        i += 1
                                     end
-
-                                    # This writes out the intermediate data every run (I think).
-                                    serialize(datafile, simulationResults)
-                                    open(datafile, "w") do io
-                                        JSON3.write(io, simulationResults)
-                                    end
-                                    i += 1
                                 end
                             end
                         end
