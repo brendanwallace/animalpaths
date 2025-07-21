39e5b302b818ce58b278aba5c418bf5b691775b6
Brendan Wallace <bwallace722@gmail.com>
2025-07-21 09:43:32 -0700

    Remove moore neighbors

--- scripts/optimality.jl
+++ scripts/optimality.jl
@@ -8,7 +8,7 @@ import Paths
 
 # Invariant.
 patchLogics = [Paths.LINEAR]
-searchStrategies = [Paths.GRID_WALK_MOORE, Paths.GRID_WALK_NEUMANN]
+searchStrategies = [Paths.GRID_WALK_NEUMANN]
 
 boundaryConditions = [Paths.SOLID, Paths.PERIODIC]
 seriesName = "optimality"
@@ -22,7 +22,7 @@ SAVE_HEADINGS = false
 SAVE_PATCHES = true
 
 
-improvementRatios: [
+improvementRatios = [
 	10.0, 20.0, 30.0, 40.0, 50.0, 60.0, 70.0, 80.0,
 	90.0, 100.0, 110.0, 120.0, 130.0, 140.0, 150.0,
 	160.0, 170.0, 180.0, 190.0, 200.0,
