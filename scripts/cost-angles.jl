970320a2e09b28f005269717726e702df30f1cc8
Brendan Wallace <bwallace722@gmail.com>
2025-07-29 13:52:29 -0700

    Set up cost-angle rerun

--- scripts/cost-angles.jl
+++ scripts/cost-angles.jl
@@ -8,7 +8,6 @@ import Paths
 
 # Invariant.
 patchLogics = [Paths.LINEAR]
-searchStrategies = [Paths.GRID_WALK_8, Paths.GRID_WALK_4] # [Paths.KANAI_SUZUKI]
 
 # costs-angles doesn't really make sense with periodic boundaries.
 boundaryConditions = [Paths.SOLID]
@@ -23,6 +22,25 @@ SAVE_HEADINGS = true
 SAVE_PATCHES = true
 
 
+
+
+include("runs.jl")
+
+# 7/29 costs run
+searchStrategies = [Paths.KANAI_SUZUKI]
+F = 20
+maxCosts = [1.25, 1.5, 2.0, 3.0, 5.0, 9.0, 17.0, 33.0, 65.0]
+improvementRatios = [100]
+PRs = [0.001]
+boundaryConditions = [Paths.SOLID]
+numLocations = [10, 20]
+NUM_REPLICAS = 4
+LOCATION_CONFIGURATIONS = [1, 2]
+
+
+"""
+
+
 # 7/21 costs run
 upf = 1000
 F = 20
@@ -32,19 +50,8 @@ PRs = [0.001]
 numLocations = [10, 20]
 NUM_REPLICAS = 4
 LOCATION_CONFIGURATIONS = [1, 2]
+searchStrategies = [Paths.GRID_WALK_8, Paths.GRID_WALK_4]
 
-include("runs.jl")
-
-"""
-# 7/3 costs run
-F = 20
-maxCosts = [1.25, 1.5, 2.0, 3.0, 5.0, 9.0, 17.0, 33.0, 65.0]
-improvementRatios = [100]
-PRs = [0.001]
-boundaryConditions = [Paths.SOLID]
-numLocations = [10, 20]
-NUM_REPLICAS = 4
-LOCATION_CONFIGURATIONS = [1, 2]
 
 
 
