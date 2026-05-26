c9b8473b292470f702f47bc1527f79283b01aefb
Brendan Wallace <brendan@Brendans-MacBook-Air.local>
2026-05-25 17:41:01 -0700

    have to save patches for svd

--- scripts/svd.jl
+++ scripts/svd.jl
@@ -14,7 +14,7 @@ seriesName = "SVD-zoom"
 SHORTEST_PATHS = false
 SAVE_PATHS = false
 SAVE_HEADINGS = false
-SAVE_PATCHES = false
+SAVE_PATCHES = true
 
 
 # 05/22/2026 rerun to fix costs subfigures
